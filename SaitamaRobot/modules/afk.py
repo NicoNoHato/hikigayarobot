f3df285c0c5937c472f0d12a678bfadfc21270ce
NicoNoHato <79739155+NicoNoHato@users.noreply.github.com>
2021-03-26 21:16:56 +0700

    Update afk.py

--- SaitamaRobot/modules/afk.py
+++ SaitamaRobot/modules/afk.py
@@ -29,14 +29,14 @@ def afk(update: Update, context: CallbackContext):
         reason = args[1]
         if len(reason) > 100:
             reason = reason[:100]
-            notice = "\nYour afk reason was shortened to 100 characters."
+            notice = "\nAlasan kamu afk telah dipersingkat dari 100 Kata."
     else:
         reason = ""
 
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} is now offline #afk{}".format(
+        update.effective_message.reply_text("{} telah afk #afk{}".format(
             fname, notice))
     except BadRequest:
         pass
@@ -57,9 +57,9 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                '{} is here!', '{} is back!', '{} is now in the chat!',
-                '{} is awake!', '{} is back online!', '{} is finally here!',
-                'Welcome back! {}', 'Where is {}?\nIn the chat!'
+                'Yo wassap {}!', 'Welkombek {}!', 'Selamat Datang kembali {}, Gada yang kangen kamu btw',
+                '{} Kenapa lu kesini woe?', '{} itu nyimakin chat ini dari tadi!', 'Yo {} Mau Mandi dulu?, Makan dulu?, Atau... @PicoKwai? >~<',
+                'Yamete...Yamete-kudasai {}-sama', 'Oh gawat! {} Gabisa bersantuy!!', 'Si {} dapet Pacar makanya dia afk tadi.', 'Selamat datang kembali {}! Tentunya dineraka ini wahaha!!', '{} disini! Mari mulai kontesnya!', '{} Keknya lu harus delete akun beb.', '{} ahem! Kesayanganku ada disini!', 'Ey {} Bangsul, Gue liat lu nyimak tapi ga nimbrung, Tabok nih (°-°).', '{} cuy kita spam di grup kuy :3', '{}Kang spam telah tiba, Siap siap kawan! Gue bakal siapin palu bannednya!', 'Yasashiku shinasai yo {} oni chan...'             
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
@@ -102,7 +102,7 @@ def reply_afk(update: Update, context: CallbackContext):
                 try:
                     chat = bot.get_chat(user_id)
                 except BadRequest:
-                    print("Error: Could not fetch userid {} for AFK module"
+                    print("Error: Gabisa dapetin userid dari {} untuk afk."
                           .format(user_id))
                     return
                 fst_name = chat.first_name
