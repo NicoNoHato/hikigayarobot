005ad23de61df74799af0a1094deba5958a1d413
dicyn <71568577+dicyn@users.noreply.github.com>
2021-06-05 04:30:30 +0700

    Update rmbg.py

--- SaitamaRobot/modules/rmbg.py
+++ SaitamaRobot/modules/rmbg.py
@@ -25,9 +25,9 @@ import requests
 from telethon import types
 from telethon.tl import functions
 
-from DaisyX.config import get_str_key
-from DaisyX.services.events import register
-from DaisyX.services.telethon import tbot
+from SaitamaRobot.config import get_str_key
+from SaitamaRobot.services.events import register
+from SaitamaRobot.services.telethon import tbot
 
 REM_BG_API_KEY = get_str_key("REM_BG_API_KEY", required=False)
 TEMP_DOWNLOAD_DIRECTORY = "./"
